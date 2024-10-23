1e6d20252f98ba4c3b6f350c487ad7d19894f8dd
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2024-10-23 23:59:13 +0900

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,4 +1,4 @@
-var manimani = require("../manimani");
+var manimani = require("../src/manimani");
 
 manimani.tokenize("隣の客はよく柿食う客だ", function(moras) {
     console.log(JSON.stringify(moras, null, 2))
