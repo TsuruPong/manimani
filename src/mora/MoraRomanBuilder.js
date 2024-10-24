6ef843710f12a9d2d49ecb92cf5980a117862ebe
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2024-10-24 12:52:22 +0900

    implementation: symbol

--- src/mora/MoraRomanBuilder.js
+++ src/mora/MoraRomanBuilder.js
@@ -16,29 +16,33 @@ MoraRomanBuilder.prototype.build = function (current, next) {
     var romans = [];
     var kana = dic.KanaRomanDictionaries.get(current);
 
-    romans = this.concat(kana.boin, kana.shiins);
-
-    if (kana.origin && kana.kogaki) {
-        var youonOrigin = this.concat(kana.origin.boin, kana.origin.shiins);
-        var youonKogaki = this.concat(kana.kogaki.boin, kana.kogaki.shiins)
-        for (const origin of youonOrigin) {
-            for (const kogaki of youonKogaki) {
-                romans.push(`${origin}${kogaki}`)
+    if (!kana) {
+        romans.push(current.toLocaleLowerCase());
+    } else {
+        romans = this.concat(kana.boin, kana.shiins);
+    
+        if (kana.origin && kana.kogaki) {
+            var youonOrigin = this.concat(kana.origin.boin, kana.origin.shiins);
+            var youonKogaki = this.concat(kana.kogaki.boin, kana.kogaki.shiins)
+            for (const origin of youonOrigin) {
+                for (const kogaki of youonKogaki) {
+                    romans.push(`${origin}${kogaki}`)
+                }
             }
         }
-    }
-
-    if (next) {
-        if (KanaDictionaries.N == current) {
-            if (!this.NArows.includes(next)) {
-                romans.push(kana.boin);
+    
+        if (next) {
+            if (KanaDictionaries.N == current) {
+                if (!this.NArows.includes(next)) {
+                    romans.push(kana.boin);
+                }
             }
-        }
-
-        if (KanaDictionaries.XTU == current) {
-            var nextKana = dic.KanaRomanDictionaries.get(next);
-            var shiins = nextKana.origin ? nextKana.origin.shiins.map(v => v.slice(0, 1)) : nextKana.shiins;
-            romans = [...romans, ...shiins];
+    
+            if (KanaDictionaries.XTU == current) {
+                var nextKana = dic.KanaRomanDictionaries.get(next);
+                var shiins = nextKana.origin ? nextKana.origin.shiins.map(v => v.slice(0, 1)) : nextKana.shiins;
+                romans = [...romans, ...shiins];
+            }
         }
     }
 
