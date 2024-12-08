11d1c116f29b30c91499e4274f4ec2c190698a01
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2024-12-08 21:57:21 +0900

    Update manimani.d.ts

--- src/types/manimani.d.ts
+++ src/types/manimani.d.ts
@@ -1,3 +1,14 @@
 declare module 'manimani' {
-    export function tokenize(dic: string, sentence: string, callback: (moras: string[]) => void): void;
+    export interface Mora {
+        from: string;
+        pos: number;
+        node: MoraNode[];
+    }
+    export interface MoraNode {
+        pos: number;
+        val: string;
+        children: MoraNode[];
+        add(node: string): void;
+    }
+    export function tokenize(dic: string, sentence: string, callback: (moras: Mora[]) => void): void;
 }
